40ab564fa973a73354ad991e579daef1716acdbd
dingyin <279293272@qq.com>
2024-07-14 16:41:22 +0800

    feat: 事件监听到button的dom上

--- react-hooks/pages/index.js
+++ react-hooks/pages/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 export default function() {
   useEffect(()=>{
-    var root = document.getElementById('rootDiv');
+    var root = document.getElementById('botton');
     // 如果你还希望在冒泡阶段处理事件，可以再添加一个处理程序
     root.addEventListener('click', function(event) {
         console.log('冒泡阶段的点击事件：', event);
@@ -22,14 +22,14 @@ export default function() {
     }, true); // 注意这里的第三个参数是true
   },[])
   return (
-    <div id='rootDiv'>
+    <div>
       <h1>class Component VS function Component</h1>
       <Class />
       <Hooks />
       <HooksTwo />
        <HooksAllApi />
        <UseLayoutEffect /> 
-      <button  onClick={()=>console.log('react 合成事件')}>dianji</button>
+      <button id='botton'  onClick={()=>console.log('react 合成事件')}>dianji</button>
     </div>
   );
 }
